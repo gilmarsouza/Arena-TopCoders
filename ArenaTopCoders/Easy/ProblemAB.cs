fc6c6773675e38690ab8b80f6b8cfd8f480cecf0
gilmarsouza <gilmar.limasou@gmail.com>
2015-08-16 10:24:46 -0300

    Exercicios do Arena Topcoders
    Em Desenvolvimento

---
+++ ArenaTopCoders/Easy/ProblemAB.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaTopCoders.Easy
+{
+    #region Problema AB: Descrição
+    /// Problem Statement
+    //      You are given two s: N and K. Lun the dog is interested in strings that satisfy the following conditions:
+    //          The string has exactly N characters, each of which is either 'A' or 'B'.
+    //          The string s has exactly K pairs (i, j) (0 <= i < j <= N-1) such that s[i] = 'A' and s[j] = 'B'.
+    //      If there exists a string that satisfies the conditions, find and return any such string. Otherwise, return an empty string.
+    #endregion
+
+    public class ProblemAb
+    {
+        /// <summary>
+        /// Método que recebe uma string com As e Bs e retorna o número de pares válidos
+        /// </summary>
+        /// <param name="cadeia">string de As e Bs. Exemplo: "BAABBBAB"</param>
+        /// <returns>retorna o número de pares válidos encontrados. Exemplo: "BAABBBAB" => 9</returns>
+        public int RetornaNumeroParesValidos(string cadeia)
+        {
+            var contador = 0;
+
+            for (int i = 0; i < cadeia.Length; i++)
+            {
+                if (cadeia[i].Equals('A'))
+                {
+                    contador += cadeia.Substring(i + 1).Replace("A", "").Length;
+                }
+            }
+
+            return contador;
+        }
+
+        /// <summary>
+        /// Método responsável por criar uma string no formato AB
+        /// com n caracteres e K pares válidos
+        /// </summary>
+        /// <param name="n">tamanho da string que será criada</param>
+        /// <param name="k">número de pares AB válidos</param>
+        /// <returns></returns>
+        public string CriaString(int n, int k)
+        {
+            var resultado = "";
+
+            if (n <= 0)
+                return resultado;
+
+            if (k <= 0)
+            {
+                resultado = "B";
+                return resultado.PadRight(n, 'A');
+            }
+
+            var metade = Convert.ToInt32(n/2);
+            var melhorCaso = metade * (n - metade);
+
+            if (k > melhorCaso)
+                return resultado;
+
+            if (k == melhorCaso)
+            {
+                resultado = resultado.PadRight(metade, 'A');
+                resultado = resultado.PadRight(n, 'B');
+
+                return resultado;
+            }
+
+            if (k < n)
+            {
+                resultado = "A" + new string('B', k);
+                resultado = resultado.PadRight(n, 'A');
+
+                return resultado;
+            }
+
+            resultado = resultado.PadRight(k / 2, 'A');
+            resultado = resultado.PadRight((k / 2) + 2, 'B');
+
+            if (k % 2 == 0)
+                resultado = resultado.PadRight(n, 'A');
+
+            if (k % 2 == 0)
+            {
+                resultado = resultado.PadRight(k / 2, 'A');
+                resultado = resultado.PadRight((k / 2) + 2, 'B');
+                resultado = resultado.PadRight(n, 'A');
+
+                return resultado;
+            }
+            else
+            {
+                resultado = "ABA";
+                resultado = resultado.PadRight(n, 'B');
+
+                return resultado;
+            }
+
+            return resultado;
+        }
+
+    }
+}
---
+++ ArenaTopCodersTestes/Easy/ProblemAbTest.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ArenaTopCoders.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArenaTopCodersTestes.Easy
+{
+    /// <summary>
+    /// Summary description for ProblemAb
+    /// </summary>
+    [TestClass]
+    public class ProblemAbTest
+    {
+        private ProblemAb problemAb;
+
+        public ProblemAbTest()
+        {
+            problemAb = new ProblemAb();
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ValidaStringComUmParValido()
+        {
+            var resultado = problemAb.RetornaNumeroParesValidos("AB");
+            Assert.AreEqual(resultado,1);
+        }
+
+        [TestMethod]
+        public void ValidaStringSemNenhumParValido()
+        {
+            var resultado = problemAb.RetornaNumeroParesValidos("BB");
+            Assert.AreEqual(resultado, 0);
+        }
+
+        [TestMethod]
+        public void ValidaStringComDoisParesValidos()
+        {
+            var resultado = problemAb.RetornaNumeroParesValidos("ABBA");
+            Assert.AreEqual(resultado, 2);
+        }
+
+        [TestMethod]
+        public void ValidaStringComDozeParesValidos()
+        {
+            var resultado = problemAb.RetornaNumeroParesValidos("BAABBABAAB");
+            Assert.AreEqual(resultado, 12);
+        }
+
+        [TestMethod]
+        public void DevolveStringVaziaNumeroSuperiorAoMelhorCaso()
+        {
+            var cadeia = problemAb.CriaString(9, 21);
+            Assert.AreEqual(cadeia,"");           
+        }
+
+        [TestMethod]
+        public void DevolveStringDeSeisCaracteresComOMaximoDeParesValidos()
+        {
+            var cadeia = problemAb.CriaString(6, 9);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 9);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeSeteCaracteresComOMaximoDeParesValidos()
+        {
+            var cadeia = problemAb.CriaString(7, 12);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 12);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeSeteCaracteresComQuatroParesValidos()
+        {
+            var cadeia = problemAb.CriaString(7, 4);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 4);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeVinteCaracteresComQuizeParesValidos()
+        {
+            var cadeia = problemAb.CriaString(20, 15);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 15);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeSeisCaracteresComOitoParesValidos()
+        {
+            var cadeia = problemAb.CriaString(6, 8);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 8);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeNoveCaracteresComQuatorzeParesValidos()
+        {
+            var cadeia = problemAb.CriaString(9, 14);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 14);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeSeisCaracteresComSeteParesValidos()
+        {
+            var cadeia = problemAb.CriaString(6, 7);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 7);
+        }
+
+        [TestMethod]
+        public void DevolveStringDeNoveCaracteresComQuizeParesValidos()
+        {
+            var cadeia = problemAb.CriaString(9, 15);
+            var resultado = problemAb.RetornaNumeroParesValidos(cadeia);
+            Assert.AreEqual(resultado, 15);
+        }
+
+
+    }
+}
